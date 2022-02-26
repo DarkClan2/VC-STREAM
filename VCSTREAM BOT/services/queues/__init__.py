a5b36b4770011ed187f8168fdb5cadc30a6e8f51
DarkClan2 <100036278+DarkClan2@users.noreply.github.com>
2022-02-26 14:06:37 +0530

    Create __init__.py

---
+++ VCSTREAM BOT/services/queues/__init__.py
@@ -0,0 +1,12 @@
+from VCPlayBot.services.queues.queues import clear 
+from VCPlayBot.services.queues.queues import get
+from VCPlayBot.services.queues.queues import is_empty
+from VCPlayBot.services.queues.queues import put
+from VCPlayBot.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
